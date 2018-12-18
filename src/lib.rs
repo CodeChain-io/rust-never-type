0f597209fb05e368dff534cb9646ef90d65a55ca
Seulgi Kim <dev@seulgi.kim>
2018-12-18 22:00:04 +0900

    Add a test to check the size of Never

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,15 @@
 
 #[derive(Debug, PartialEq)]
 pub enum Never {}
+
+#[cfg(test)]
+mod tests {
+    use std::mem::size_of;
+
+    use super::*;
+
+    #[test]
+    fn size_of_never_is_zero() {
+        assert_eq!(0, size_of::<Never>());
+    }
+}
